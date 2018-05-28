129e3b348d1572c2975882f8edce6e669ba36007
Aarush Verma <aarushverma96@gmail.com>
2018-05-29 00:29:16 +0530

    Printing largest sum subarray

--- dp/largest_sum_subarray.cpp
+++ dp/largest_sum_subarray.cpp
@@ -9,13 +9,27 @@ int largeSum(vector<int> A)
   int len=A.size();
   int currMax=A[0];
   int maxFar=A[0];
-
+   s=e=0;
   for (int i = 1; i < len; i++)
   {
-    
-    currMax=max(A[i],currMax+A[i]);
-    maxFar=max(maxFar,currMax);
+    if(A[i]>currMax+A[i])
+    {
+      s=i;
+      currMax=A[i];
+    }
+    else
+    {
+      currMax=currMax+A[i];
+    }
+
+    if(currMax>maxFar)
+    {
+      e=i;
+      maxFar=currMax;
+    }
+
   }
+  cout << A[s]<< " " << A[e];
   return maxFar;
 }
 
