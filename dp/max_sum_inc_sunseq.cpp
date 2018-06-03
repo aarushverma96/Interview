68938d729469e464a23b40c362e9b47b6c714bf2
Aarush Verma <aarushverma96@gmail.com>
2018-06-03 13:54:07 +0530

    maximum sum inc

--- dp/max_sum_inc_sunseq.cpp
+++ dp/max_sum_inc_sunseq.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
-int main()
-{
-  return 0;
-}
 
 int maxSum(vector<int> A)
 {
   int n=A.size();
+  cout << n;
   int ans[n];
 
-  memset(ans,-1,sizeof(ans));
+//  memset(ans,-1,sizeof(ans));
+  for (int i = 0; i < n; i++) {
+    ans[i]=A[i];
+  }
+  cout << endl;
 
   ans[0]=A[0];
 
@@ -20,12 +21,18 @@ int maxSum(vector<int> A)
   {
     for (int j = 0; j < i; j++)
     {
-      if(A[i]<A[j] && ans[i]<ans[j]+A[i]);
+      if(A[i]>A[j] && ans[i]<ans[j]+A[i]);
       {
         ans[i]=ans[j]+A[i];
       }
     }
   }
+
+  for (int i = 0; i < n; i++) {
+    cout << ans[i] << " " ;
+  }
+   cout << endl;
+
 
   int max=INT_MIN;
 
@@ -39,4 +46,14 @@ int maxSum(vector<int> A)
 
   return max;
 
+}
+
+
+int main()
+{
+  int a[7]={1,101,2,3,100,4,5};
+  std::vector<int> v;
+  copy(&a[0],&a[7],back_inserter(v));
+  cout << maxSum(v) << endl;
+  return 0;
 }
