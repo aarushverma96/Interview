be668c8619851727b3f68f535dc6a8531ff8e043
Aarush Verma <aarushverma96@gmail.com>
2018-06-05 23:11:24 +0530

    gg

---
+++ dp/gold_mine.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  return 0;
+}
+
+int maxGold(std::vector<int> gold[],int row)
+{
+  int g[col+1][gold[0].size()];
+  memset(g,0,sizeof(g));
+
+  for (int i = golg[0].size(); i>=0; i--)
+  {
+    for (int j = 0; j <=col ; j++)
+    {
+      
+    }
+  }
+
+
+}
