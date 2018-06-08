2c7f11de05368d8ac6983422d40989a52c841f1f
Aarush Verma <aarushverma96@gmail.com>
2018-06-09 03:19:42 +0530

    Lowest common ancestor(Tushar Roy)

---
+++ Trees/LCA_in_BT.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef struct Tree
+{
+  int val;
+  struct Tree* left;
+  struct Tree* right;
+}Tree;
+
+int main()
+{
+  return 0;
+}
+
+
+Tree* LCA(Tree* root,Tree* n1,Tree* n2)
+{
+  if(root==NULL)//no Tree
+  {
+    return NULL;
+  }
+
+  if(root==n1 || root==n2)
+  {
+    return root;
+  }
+
+  Tree* left=LCA(root->left,n1,n2);
+  Tree* right=LCA(root->right,n1,n2);
+
+  if(left==NULL)
+  {
+    return right;
+  }
+  if(right==NULL)
+  {
+    return left;
+  }
+
+  return root;
+
+
+}
