f38bca1e103dd26e41ca5c0f321364a3189757fd
Aarush Verma <aarushverma96@gmail.com>
2018-06-12 22:57:45 +0530

    Counts paths from a point to reach Origin

---
+++ dp/paths_origin.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int paths(int n,int m)
+{
+  int dp[n+1][m+1];
+
+  for (int i = 0; i <=n ; i++)
+  {
+    dp[i][0]=1;
+  }
+
+
+  for (int i = 0; i <=m ; i++)
+  {
+    dp[0][i]=1;
+  }
+
+  for (int i = 1; i <= n; i++)
+  {
+    //cout <<"hehe"<<endl;
+    for (int j = 1; j <= m; j++)
+    {
+    //  cout << "h" << endl;
+      dp[i][j]=dp[i-1][j]+dp[i][j-1];
+    }
+  }
+
+
+  return dp[n][m];
+}
+
+int main()
+{
+
+  cout << paths(3,6) << endl;
+  return 0;
+}
